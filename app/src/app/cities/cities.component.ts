fd03eee69154ce8e781c23a5b63ef25afbcc9add
Douglas Santanna <douglbb1@gmail.com>
2022-01-31 21:15:01 -0300

    first commit

---
+++ app/src/app/cities/cities.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ct-cities',
+  templateUrl: './cities.component.html',
+  styleUrls: ['./cities.component.css']
+})
+export class CitiesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
